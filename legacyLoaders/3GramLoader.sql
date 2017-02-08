a871105bc564fb99206c795d1a4f3595d534fa9c
Nate Watson <watsonn2013@my.fit.edu>
2017-02-08 12:11:37 -0500

    Revert "QueryBuilder"
    This reverts commit 0d21e821fb6474a84f74a5b4b3cd0623540a21c8.

--- legacyLoaders/3GramLoader.sql
+++
@@ -1,2 +0,0 @@
-USE `NGrams`;
-LOAD DATA LOCAL INFILE 'C:\\Users\\natew\\Desktop\\LegendaryAI\\n-gram-3.csv' INTO TABLE `3Grams` FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r' (`Count`,`Word1`,`Word2`,`Word3`) SET `Index` = 0, `Percent`=0.0;
--- legacyLoaders/4GramLoader.sql
+++
@@ -1,4 +0,0 @@
-USE `NGrams`;
-/*LOAD DATA LOCAL INFILE 'C:\\Users\\natew\\Desktop\\LegendaryAI\\n-gram-4.csv' INTO TABLE `4Grams` FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r' (`Count`,`Word1`,`Word2`,`Word3`,`Word4`) SET `Index` = 0, `Percent`=0.0;*/
-/*SELECT COUNT(*) FROM `4Grams`;
-DELETE FROM `4Grams` WHERE `Index`= 1034308;*/
