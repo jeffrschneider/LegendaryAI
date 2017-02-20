da8c95ef590b48fefe7031a3384bbfb5aca13e9a
Nate Watson <watsonn2013@my.fit.edu>
2017-02-20 14:27:50 -0500

    Usable Server
    It works to the extent that results are given.

--- jobServer.js
+++ jobServer.js
@@ -1,47 +1,67 @@
 "use strict";
-const spawn = require("child_process").spawn;
+const exec = require("child_process").exec;
 const https = require("https");
 const fs = require('fs');
 const databaseConfig = JSON.parse(fs.readFileSync(process.argv[2], "utf8"));
 const launcherString =
-  ("java CorrectionInstance " + ((databaseConfig.user + " ") + (databaseConfig.password + " "))) +
+  ("java -jar usableInstance.jar " + ((databaseConfig.user + " ") + (databaseConfig.password + " "))) +
   ((databaseConfig.host + " ") + (databaseConfig.database + " "));
 const serverConfig = {
   key: fs.readFileSync('./key.pem'),
-  cert: fs.readFileSync('./cert.pem')
+  cert: fs.readFileSync('./cert.pem'),
+  passphrase: "D3@dH00k3r"
 };
 const errString = "{'statusCode': -1, 'err':'unable to parse request'}";
 https.createServer(serverConfig, function(request, response){
   var body = [];
   request.on('error', function(err) {
-    response.write(errString);
-    response.end();
+    if (!(response.finished)) {
+      response.write(errString);
+      response.end();
+    }
+    console.log("network error");
   }).on('data', function(chunk) {
     body.push(chunk);
   }).on('end', function() {
     body = Buffer.concat(body).toString();
     try {
-      JSON.parse(body);
+      const input = JSON.parse(body);
       var correction = "";
-      const proc = spawn(launcherString + body.flawedString);
-      proc.stderr.on("data", function(data){
-        response.write(errString);
-        response.end();
-      });
-      proc.stdout.on("data", function(data){
-        correction = correction + data;
-      });
-      proc.on('close', function(term){
+      const spawnString = launcherString + input.flawedString.split("{").join(" ").split("}").join(" ");
+      exec(spawnString, function(error, stdout, stderr){
+        if (error){
+          if (!(response.finished)) {
+            response.write(errString);
+            response.end();
+          }
+          console.log("reciever error: " + spawnString);
+          console.log(error);
+          return;
+        }
+        if (stderr){
+          if (!(response.finished)) {
+            response.write(errString);
+            response.end();
+          }
+          console.log("reciever error: " + spawnString);
+          console.log(stderr);
+          return;
+        }
         var responseData = {
           statusCode: 1,
-          result: correction
+          result: stdout
         };
-        response.write(JSON.stringify(responseData));
-        response.end();
+        if (!(response.finished)) {
+          response.write(JSON.stringify(responseData));
+          response.end();
+        }
       });
     } catch (e) {
-      response.write(errString);
-      response.end();
+      if (!(response.finished)) {
+        response.write(errString);
+        response.end();
+      }
+      console.log("JSON parsing error");
     }
   });
 }).listen(8080);
